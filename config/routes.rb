e62e8a7cd95965cbb8461f12be94253c4e900aef
emmanuelle jolu <emmanuelle.jolu.ej@gmail.com>
2016-11-08 15:59:31 +0100

    task manager done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # see http://guides.rubyonrails.org/routing.html
   root "tasks#index"
-  get "tasks" => "tasks#index"
+
+  resources :tasks
+
+  # get "tasks/new" => "tasks#new", :as => 'new_task'
+  # post "tasks" => "tasks#create"
+
+  # get "tasks" => "tasks#index"
+  # get "tasks/:id" => "tasks#show", :as => 'task'
+
+  # get "tasks/:id/edit" => "tasks#edit", :as => 'edit_task'
+  # patch "tasks/:id" => "tasks#update"
+
+  # delete "tasks/:id" => "tasks#destroy", :as => 'delete_task'
 end
